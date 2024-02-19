c38529fe9f544b2fa3310e40e1eb3c1889ec8166
NamaKa298 <marion.saint-martin@protonmail.com>
2024-02-19 13:52:32 +0000

    Projet 9 task 6 Afficher les differents type test 1

---
+++ hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (Success)
+**/
+int main(void)
+{
+        printf("Size of a char: %d\n",sizeof(char));
+	printf("Size of an int: %d\n",sizeof(int));
+	printf("Size of a long int: %d\n",sizeof(long));
+	printf("Size of a long long int: %d\n",sizeof(long long));
+	printf("Size of a float: %d\n",sizeof(float));
+        return (0);
+}
