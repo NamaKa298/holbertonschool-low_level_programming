53d7dd3c666d70ba36d209c53350c928428436df
NamaKa298 <marion.saint-martin@protonmail.com>
2024-02-20 06:52:00 -0800

    Afficher valeur de 0 à 9

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int chif;
+
+	for (chif = 0 ; chif <= 9 ; chif++)
+	{
+		printf("%d", chif);
+	}
+	printf("\n");
+	return (0);
+}
