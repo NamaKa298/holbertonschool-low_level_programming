17088237f56411d7491de84fde01a10ee0257baf
NamaKa298 <marion.saint-martin@protonmail.com>
2024-02-20 04:46:09 -0800

    Projet 10 task 2 utilisation de putchar pour liste alphabet test 2

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,6 +1,5 @@
 #include<stdio.h>
-/*
- *
+/**
  * main - Entry point
  *
  * Return: Always 0 (Success)
@@ -13,6 +12,7 @@ int main(void)
 	for (ch = 'a' ; ch <= 'z' ; ch++)
 	{
 		putchar(ch);
+		putchar('\n');
 	}
 	return (0);
 }
