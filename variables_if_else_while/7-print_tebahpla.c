bc01a4e0b8867bd38a02ef20cff96afe23af1016
NamaKa298 <marion.saint-martin@protonmail.com>
2024-02-20 07:27:04 -0800

    Alphabet inversé avec putchar

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z' ; ch >= 'a' ; ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
