ef2abd633e62d0093f7c7c499389561e9e2469d7
NamaKa298 <marion.saint-martin@protonmail.com>
2024-02-20 07:11:19 -0800

    Afficher valeur de 0 à 9 en utilisant code ASCII

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int chif;
+
+	for (chif = 48 ; chif <= 57 ; chif++)
+	{
+		putchar(chif);
+	}
+	putchar('\n');
+	return (0);
+}
