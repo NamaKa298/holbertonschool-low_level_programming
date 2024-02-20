59d5c71fda4c6623ea8c57104dac35856987caa6
NamaKa298 <marion.saint-martin@protonmail.com>
2024-02-20 03:01:29 -0800

    Projet 10 task 1 Utilisation de rand et du modulo test 4

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -15,7 +15,7 @@ int main(void)
 	ld = n % 10;
 	if (ld > 5)
 	{
-		printf("Last digit of %d is %d and is greather than 5\n", n, ld);
+		printf("Last digit of %d is %d and is greater than 5\n", n, ld);
 	}
 	if (ld == 0)
 	{
