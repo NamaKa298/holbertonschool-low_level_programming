d506487eb105dfa22310a98d3cf27ebf3011a3f1
NamaKa298 <marion.saint-martin@protonmail.com>
2024-02-21 12:55:01 -0800

    alphabet dans ordre

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include"main.h"
+
+/**
+ * print_alphabet - me donne l'alphabet dans l'ordre
+ *
+ * Return: Nothing
+ */
+
+void print_alphabet(void)
+{
+	char let;
+
+	for (let = 'a' ; let < 'z' ; let++)
+	_putchar(let);
+	_putchar('\n');
+}
