f611387eec9d6b27531e85262065818c0dd56f82
NamaKa298 <marion.saint-martin@protonmail.com>
2024-02-23 00:44:28 -0800

    Fonction somme

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * add - check the code
+ *
+ * @a: character to be verified
+ * @b: character to be verified
+ *
+ * Return: somme.
+ */
+int add(int a, int b)
+{
+	int somme;
+
+	somme = a + b;
+	return (somme);
+}
