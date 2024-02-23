4313c1fefef13495640c23c55a78561e0105ec97
NamaKa298 <marion.saint-martin@protonmail.com>
2024-02-23 13:08:46 -0800

    somme des multiples de 3 et 5 jusqu_a 1023

--- functions_nested_loops/101-natural.c
+++ functions_nested_loops/101-natural.c
@@ -10,6 +10,9 @@ int main(void)
 {
 	int nombre_max = 1023;
 	int valeur_a_garder, somme, i;
+
+	somme = 0;
+	valeur_a_garder = 0;
 		for (i = 0 ; i <= nombre_max ; i++)
 		{
 			if (i % 3 == 0 || i % 5 == 0)
