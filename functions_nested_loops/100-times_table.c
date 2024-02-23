585be324d3b247a934a0e510407559e7528fe192
NamaKa298 <marion.saint-martin@protonmail.com>
2024-02-23 04:53:47 -0800

    Table de 100

--- functions_nested_loops/100-times_table.c
+++ functions_nested_loops/100-times_table.c
@@ -11,7 +11,7 @@ void print_times_table(int n)
 {
 	int i, j, k, l;
 
-	if (n < 32)
+	if (n < 15 && n > 0)
 	{
 		for (i = 0 ; i <= n ; i++)
 		{
@@ -60,7 +60,7 @@ void print_times_table(int n)
 			_putchar('\n');
 		}
 	}
-	if (n > 32)
+	if (n > 15 && n < 0)
 	{
 		_putchar('\n');
 	}
