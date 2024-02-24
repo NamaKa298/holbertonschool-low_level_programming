82fe1a7693c86415724585d4578a960603bd5835
NamaKa298 <marion.saint-martin@protonmail.com>
2024-02-24 05:42:30 -0800

    Fibonacci les 50 premières valeurs

--- functions_nested_loops/102-fibonacci.c
+++ functions_nested_loops/102-fibonacci.c
@@ -12,14 +12,23 @@ int main(void)
 	terme1 = 1;
 	terme2 = 2;
 
-	printf("%ld", terme1);
-	printf(", %ld", terme2);
-	for (pas = 0 ; pas < 48 ; pas++)
+	for (pas = 1 ; pas < 51 ; pas++)
 	{
-		terme3 = terme1 + terme2;
-		printf(", %ld", terme3);
-		terme1 = terme2;
-		terme2 = terme3;
+		if (pas == 1)
+		{
+			printf("%ld", terme1);
+		}
+		else if (pas == 2)
+		{
+			printf("%ld", terme2);
+		}
+		else
+		{
+			terme3 = terme1 + terme2;
+			printf(", %ld", terme3);
+			terme1 = terme2;
+			terme2 = terme3;
+		}
 	}
 	printf("\n");
 	return ('0');
