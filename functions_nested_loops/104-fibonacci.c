f8bcbae007f41d423cc2669fa46fafba164ab675
NamaKa298 <marion.saint-martin@protonmail.com>
2024-02-25 11:50:54 -0800

    Fibonacci 100 premiers termes

---
+++ functions_nested_loops/104-fibonacci.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	unsigned long int terme1, terme2, terme3;
+	int pas;
+
+	terme1 = 1;
+	terme2 = 2;
+
+	printf("%lu, %lu", terme1, terme2);
+
+	for (pas = 0 ; pas <= 95 ; pas++)
+	{
+		terme3 = terme1 + terme2;
+		printf(", %lu", terme3);
+		terme1 = terme2;
+		terme2 = terme3;
+	}
+	printf("\n");
+	return (0);
+}
