4d525a5759ee7334c8ba97618812fb134e8a058f
NamaKa298 <marion.saint-martin@protonmail.com>
2024-02-26 02:53:00 -0800

    Affiche diagonale

---
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,24 @@
+#include"main.h"
+
+/**
+ * print_diagonal - check the code
+ *
+ * @n: character to be verified
+ *
+ * Return: Always nothing.
+ */
+void print_diagonal(int n)
+{
+	int i, j;
+
+	for (i = 0 ; i < n ; i++)
+	{
+		for (j = 0 ; j <= i ; j++)
+		{
+			_putchar(' ');
+		}
+		_putchar(92);
+		_putchar('\n');
+	}
+	_putchar('\n');
+}
