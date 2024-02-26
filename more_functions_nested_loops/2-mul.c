2765a7444eb3fbba8dd13c01cfb7668d40bbfc59
NamaKa298 <marion.saint-martin@protonmail.com>
2024-02-26 01:01:06 -0800

    Retourne la multiplication de 2 valeur

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include"main.h"
+#include<stdio.h>
+#include <ctype.h>
+/**
+ * mul - check the code.
+ *
+ * @a: The character to print
+ * @b: The character to print
+ *
+ * Return: a * b.
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
