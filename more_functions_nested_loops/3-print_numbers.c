a62daed598c9059d69cda62c93b3aaa768f683dd
NamaKa298 <marion.saint-martin@protonmail.com>
2024-02-26 01:18:59 -0800

    Affiche les 10 premiers chiffres

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include"main.h"
+/**
+ * print_numbers - check the code
+ * _putchar - check the code
+ *
+ * Return: void.
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0 ; i <= 9 ; i++)
+	{
+		_putchar('0' + i);
+	}
+	_putchar('\n');
+}
