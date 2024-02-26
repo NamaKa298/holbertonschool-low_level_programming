e514689cc3241ad1bfd2ed12e3ccbc8abc4a5fc6
NamaKa298 <marion.saint-martin@protonmail.com>
2024-02-26 01:59:57 -0800

    10 lignes de valeurs allant de 0 à 14

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include"main.h"
+/**
+ * more_numbers - check the code
+ *
+ * Return: Always 0.
+ */
+void more_numbers(void)
+{
+	int i, j;
+
+	for (i = 0 ; i <= 10 ; i++)
+	{
+		for (j = 0 ; j <= 14 ; j++)
+		{
+			if (j >= 10)
+			{
+				_putchar('0' + j / 10);
+			}
+			_putchar('0' + j % 10);
+		}
+		_putchar('\n');
+	}
+}
