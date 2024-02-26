7b070660f4c3a64e9cf23071d29f15244c5a4a3c
NamaKa298 <marion.saint-martin@protonmail.com>
2024-02-26 02:15:45 -0800

    Afficher le nombre de _ correspondant à la valeur en entrée

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include"main.h"
+/**
+ * print_line - check the code
+ *
+ * @n: The character to print
+ * Return: Always 0.
+ */
+void print_line(int n)
+{
+	int i;
+
+	for (i = 0 ; i < n ; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
+
