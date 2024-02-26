3f999df9d6e59aee808d788e74a8780814e6784d
NamaKa298 <marion.saint-martin@protonmail.com>
2024-02-26 04:54:30 -0800

    Fizz-Buzz test

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+/**
+ * main - check the code
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 1 ; i <= 100 ; i++)
+	{
+		if (i == 1)
+		{
+			printf("%d", i);
+		}
+		else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf(" FizzBuzz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf(" Buzz");
+		}
+		else if (i % 3 == 0)
+		{
+			printf(" Fizz");
+		}
+		else
+		{
+			printf(" %d", i);
+		}
+	}
+	printf("\n");
+	return (0);
+}
