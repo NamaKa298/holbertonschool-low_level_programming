243b8241834ab7baab2c5d83c7a10340e2c9a8e5
NamaKa298 <marion.saint-martin@protonmail.com>
2024-02-27 07:38:56 -0800

    Imprime 1 caractère sur 2

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * puts2 - check the code
+ *
+ * @str: The character to print
+ *
+ * Return: Nothing.
+ */
+void puts2(char *str)
+{
+	int longueur, i;
+
+	longueur = 0;
+
+	while (str[longueur] != '\0')
+	{
+		longueur += 1;
+	}
+	for (i = 0 ; i <= longueur ; i++)
+	{
+		if (i == 0 || i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
+	}
+	_putchar('\n');
+}
