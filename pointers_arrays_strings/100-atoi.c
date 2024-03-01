0bdc687f971993fb3a05048111b580eab50dc4a9
root <root@DESKTOP-TTQU7NC>
2024-03-01 20:01:57 +0100

    creation fonction atoi

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -4,29 +4,44 @@
  *
  * @s: character to be verified
  *
- * Return: Always result.
+ * Return: result
  */
+#include <stdio.h>
+
 int _atoi(char *s)
 {
-	int i = 0;
-	int sign = 1;
-	int res = 0;
+        int i = 0;
+        int sign = 0;
+        int res = 0;
 
-	while (s[i])
-	{
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			res = res * 10 + (s[i] - '0');
-		}
-		else if (s[i] == '-')
-		{
-			sign *= -1;
-		}
-		else if (res != 0)
-		{
-			break;
-		}
-		i++;
-	}
-	return (res * sign);
+        while (s[i])
+        {
+                if (s[i] == ' ' && s[i - 1] >= '0' && s[i - 1] <= '9')
+                {
+                        return (res);
+                }
+                else if ((s[i] < '0' || s[i] > '9') && (s[i] != '-'))
+                {
+                        i++;
+                        continue;
+                }
+                else if (s[i] == '-')
+                {
+                        sign++;
+                        i++;
+                }
+                else if (s[i] >= '0' && s[i] <= '9')
+                {
+                        res = res * 10 + s[i] - '0';
+                        i++;
+                }
+        }
+        if (sign % 2 != 0)
+        {
+                return (-res);
+        }
+        else
+        {
+                return (res);
+        }
 }
