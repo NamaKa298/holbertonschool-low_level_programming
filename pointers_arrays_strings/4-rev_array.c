bd106702b730e8c72471c3e73598cf42110c2a0c
root <root@DESKTOP-TTQU7NC>
2024-03-01 16:05:27 +0100

    reverse_array

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * reverse_array - print an array of integers
+ *
+ * @a: an array of integers
+ * @n: the number of elements to swap
+ *
+ * Return: nothing.
+ */
+void reverse_array(int *a, int n)
+{
+        int i, temporaire;
+
+        for (i = 0 ; i < n / 2 ; i++)
+        {
+                temporaire = a[i];
+                a[i] = a[n - i - 1];
+                a[n - i - 1] = temporaire;
+        }
+}
