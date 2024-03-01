5e069f20bb6e3bd27a38f3f600f5cac8a44bb2d3
root <root@DESKTOP-TTQU7NC>
2024-03-01 22:24:24 +0100

    creation fonction atoi

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -2,42 +2,42 @@
 #include <limits.h>
 
 /**
- 
-_atoi - Entry point.
-@s: character to be verified.
-*
+
+  _atoi - Entry point.
+  @s: character to be verified.
+ *
 Return: convert integer.
 */
 int _atoi(char *s)
 {
-        int i = 0;
-        int sign = 1;
-        int result = 0;
-        int digit;
-        int max_div_10 = INT_MAX / 10;
-        int max_mod_10 = INT_MAX % 10;
+	int i = 0;
+	int sign = 1;
+	int result = 0;
+	int digit;
+	int max_div_10 = INT_MAX / 10;
+	int max_mod_10 = INT_MAX % 10;
 
-        while (s[i])
-        {
-                if (s[i] == '-')
-                        sign *= -1;
-                if (s[i] >= '0' && s[i] <= '9')
-                {
-                        digit = s[i] - '0';
+	while (s[i])
+	{
+		if (s[i] == '-')
+			sign *= -1;
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			digit = s[i] - '0';
 
-                        if (result > max_div_10 || (result == max_div_10 && digit > max_mod_10))
-                        {
-                                return (sign == 1 ? INT_MAX : INT_MIN);
-                        }
+			if (result > max_div_10 || (result == max_div_10 && digit > max_mod_10))
+			{
+				return (sign == 1 ? INT_MAX : INT_MIN);
+			}
 
-                        result = result * 10 + digit;
-                }
-                else if (result > 0)
-                {
-                        break;
-                }
-                i++;
-        }
+			result = result * 10 + digit;
+		}
+		else if (result > 0)
+		{
+			break;
+		}
+		i++;
+	}
 
-        return (result * sign);
+	return (result * sign);
 }
