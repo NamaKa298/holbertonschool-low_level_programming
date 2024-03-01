854679119f60ea80dbf2a6dfc3e73635f1b5fd49
root <root@DESKTOP-TTQU7NC>
2024-03-01 15:24:51 +0100

    _strncpy

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -13,16 +13,13 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0 ; i < n ; i++)
+	for (i = 0 ; i < n && src[i] != '\0'; i++)
 	{
-		if (src[i] != '\0')
-		{
-			dest[i] = src[i];
-		}
-		else
-		{
-			dest[i] = '\0';
-		}
+		dest[i] = src[i];
+	}
+	for ( ; i < n ; i++)
+	{
+		dest[i] = '\0';
 	}
 	return (dest);
 }
