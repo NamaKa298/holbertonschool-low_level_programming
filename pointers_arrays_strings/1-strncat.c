05953c643dc49d74c3654b69b06ab32088d738a1
root <root@DESKTOP-TTQU7NC>
2024-03-01 14:47:37 +0100

    Concatener 2 chaines de carctères 2

---
+++ pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include"main.h"
+/**
+ * _strncat - check the code
+ *
+ * @dest: character to be verified
+ * @src: character to be verified
+ * @n: character to be verified
+ * Return: Always dest.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+        int i = 0;
+	int j;
+
+        while (dest[i])
+        {
+                i++;
+        }
+        for (j = 0 ; j < n ; j++)
+        {
+                dest[i] = src[j];
+
+                i++;
+        }
+        return (dest);
+}
