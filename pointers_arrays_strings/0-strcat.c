521386bbc5a86c332330665ab98fc46a65a71a29
root <root@DESKTOP-TTQU7NC>
2024-03-01 14:20:08 +0100

    Concaténation

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -2,23 +2,25 @@
 /**
  * _strcat - check the code
  *
+ * @dest: character to be verified
+ * @src: character to be verified
  * Return: Always dest.
  */
 char *_strcat(char *dest, char *src)
 {
-        int i  = 0;
-        int j = 0;
+	int i = 0;
+	int j = 0;
 
-        while(dest[i])
-        {
-               i++;
-        }
-        while ( src[j])
-        {
-                dest[i] = src[j];
+	while (dest[i])
+	{
+		i++;
+	}
+	while (src[j])
+	{
+		dest[i] = src[j];
 
 		i++;
-                j++;
-        }
-        return (dest);
+		j++;
+	}
+	return (dest);
 }
