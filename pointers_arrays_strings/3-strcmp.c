4408934d70315f4e6b9a60f01143f803a76fdef5
root <root@DESKTOP-TTQU7NC>
2024-03-01 15:49:22 +0100

    création de strcmp

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * _strcmp - check the code
+ *
+ *
+ * Return: Always 0.
+ */
+int _strcmp(char *s1, char *s2)
+{
+	int i, result;
+
+	result = 0;
+
+	for (i = 0 ; s1[i] != '\0' ; i++)
+	{
+		if(s1[i] == s2[i])
+		{
+			continue;
+		}
+		else
+		{
+			result = s1[i] - s2[i];
+			return (result);
+		}
+	}
+	return (result);
+}
