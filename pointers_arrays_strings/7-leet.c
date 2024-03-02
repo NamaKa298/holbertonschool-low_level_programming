b97a3f6e335de2e13c89574c64cbc7a76654a5eb
root <root@DESKTOP-TTQU7NC>
2024-03-02 17:48:11 +0100

    Phrase encodée en 1337

--- pointers_arrays_strings/7-leet.c
+++ pointers_arrays_strings/7-leet.c
@@ -8,29 +8,20 @@
  */
 char *leet(char *str)
 {
-	int i;
+	int i, j;
 
-	for(i=0 ; str[i] != '\0' ; i++)
+	char letr_min[] = "aeotl";
+	char letr_maj[] = "AEOTL";
+	char nbre[] = "43071";
+
+	for (i = 0 ; str[i] != '\0' ; i++)
 	{
-		if (str[i] == 'a' || str[i] == 'A')
+		for (j = 0 ; letr_min[j] != '\0' ; j++)
 		{
-			str[i] = '4';
-		}
-		else if (str[i] == 'e' || str[i] == 'E')
-		{
-			str[i] = '3';
-		}
-		else if (str[i] == 'o' || str[i] == 'O')
-		{
-			str[i] = '0';
-		}
-		else if (str[i] == 't' || str[i] == 'T')
-		{
-			str[i] = '7';
-		}
-		else if (str[i] == 'l' || str[i] == 'L')
-		{
-			str[i] = '1';
+			if (str[i] == letr_min[j] || str[i] ==  letr_maj[j])
+			{
+				str[i] = nbre[j];
+			}
 		}
 	}
 	return(str);
