ecac5138515c8ea4225248443159bfa769083596
root <root@DESKTOP-TTQU7NC>
2024-03-02 18:30:23 +0100

    fonction atoi

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -13,29 +13,29 @@ int _atoi(char *s)
 	int i = 0;
 	int sign = 1;
 	int result = 0;
-	int digit;
+	int unite;
 	int max_div_10 = INT_MAX / 10;
 	int max_mod_10 = INT_MAX % 10;
 
 	while (s[i])
 	{
-		if (s[i] == '-')
-		{
-			sign *= -1;
-		}
 		if (s[i] >= '0' && s[i] <= '9')
 		{
-			digit = s[i] - '0';
-			if (result > max_div_10 || (result == max_div_10 && digit > max_mod_10))
+			unite = s[i] - '0';
+			if (result > max_div_10 || (result == max_div_10 && unite > max_mod_10))
 			{
 				return (sign == 1 ? INT_MAX : INT_MIN);
 			}
-			result = result * 10 + digit;
+			result = result * 10 + unite;
 		}
 		else if (result > 0)
 		{
 			break;
 		}
+		else if (s[i] == '-')
+                {
+                        sign *= -1;
+                }
 		i++;
 	}
 	return (result * sign);
