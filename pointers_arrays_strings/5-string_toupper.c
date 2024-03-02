62d836a86005b55fcda17b334654f85aef46c4d7
root <root@DESKTOP-TTQU7NC>
2024-03-02 14:59:23 +0100

    minuscule en majuscule

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -11,9 +11,9 @@ char *string_toupper(char *str)
 {
 	int i;
 
-	for(i = 0 ; str[i] != 0 ; i++)
+	for (i = 0 ; str[i] != 0 ; i++)
 	{
-		if( str[i] >= 97 && str[i] <= 122)
+		if (str[i] >= 97 && str[i] <= 122)
 		{
 			str[i] = str[i] - 32;
 		}
