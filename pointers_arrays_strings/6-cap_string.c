3d57b7a08cc9e9ea8e697ab1b55cc310afdc2f16
root <root@DESKTOP-TTQU7NC>
2024-03-02 16:16:38 +0100

    Première lettre en majuscule

---
+++ pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * cap_string - check the code
+ *
+ * @str: The character to print
+ *
+ * Return: Always 0.
+ */
+char *cap_string(char *str)
+{
+	int i;
+
+	for (i =  0 ; str[i] != '\0' ; i++)
+	{
+		if (str[i] >= 97 && str[i] <= 122)
+		{
+			if ((str[i - 1] >= 65 && str[i - 1] <= 90) || (str[i - 1] >= 97 && str[i - 1] <= 122) ||(str[i - 1] >= 48 && str[i - 1] <= 57))
+			{
+				continue;
+			}
+			else
+			{
+				str[i] = str[i] - 32;
+			}
+		}
+	}
+	return (str);
+}
