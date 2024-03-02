4ef0743acf1d9a6018e3e4be136b1b0041f3321c
root <root@DESKTOP-TTQU7NC>
2024-03-02 20:21:16 +0100

    Combinaisons de deux fois deux chiffres

---
+++ variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i, j, k, l;
+
+	for (i = 0 ; i <= 9 ; i++)
+	{
+		for (j = 0 ; j <= 8 ; j++)
+		{
+			for (k = 0 ; k <= 9 ; k++)
+			{
+				for (l = 1 ; l <= 9 ; l++)
+				{
+					if (k < i || l <= j)
+					{
+						continue;
+					}
+					else
+					{
+						if (i != 0 || j != 0 || k != 0 || l != 1)
+						{
+							putchar(',');
+							putchar(' ');
+						}
+						putchar('0' + i);
+						putchar('0' + j);
+						putchar(' ');
+						putchar('0' + k);
+						putchar('0' + l);
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
