d4a34b6568a3b52662fb0762d488e6d5e4a9a39f
root <root@DESKTOP-TTQU7NC>
2024-03-02 20:05:37 +0100

    Combinaisons de trois chiffres

---
+++ variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+/**
+ * main - check the code.
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 0 ; i <= 7 ; i++)
+	{
+		for (j = 1 ; j <= 8 ; j++)
+		{
+			for ( k = 2 ; k <= 9 ; k++)
+			{
+				if (j <= i || k <= j)
+				{
+					continue;
+				}
+				else
+				{
+					if (i != 0 || j != 1 || k != 2)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+					putchar('0' + i);
+					putchar('0' + j);
+					putchar('0' + k);
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
