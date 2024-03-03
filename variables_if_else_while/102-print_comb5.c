db94023031317840543d66ee54012a57735d0011
root <root@DESKTOP-TTQU7NC>
2024-03-03 15:51:16 +0100

    Combinaisons de deux fois deux chiffres

--- variables_if_else_while/102-print_comb5.c
+++ variables_if_else_while/102-print_comb5.c
@@ -16,7 +16,7 @@ int main(void)
 			{
 				for (l = 0 ; l <= 9 ; l++)
 				{
-					if ((k <= i && l < j) || (i == k && j >= l))
+					if ((k <= i && l < j) || (i == k && j >= l) || ( i > k && j <= l))
 					{
 						continue;
 					}
