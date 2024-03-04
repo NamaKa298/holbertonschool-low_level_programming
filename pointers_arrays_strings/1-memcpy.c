aab7ffa60c9a1adc77dbe9017bbbcec9196bb2e5
root <root@DESKTOP-TTQU7NC>
2024-03-04 10:58:04 +0100

    changement des valeurs 50 à 60

---
+++ pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _memset - check the code
+ *
+ * @dest: The character to print
+ * @src: The character to print
+ * @n: The character to print
+ *
+ * Return: character.
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+        unsigned int i;
+
+        for (i = 0 ; i < n ; i++)
+        {
+                dest[i] = src[i];
+        }
+        return (dest);
+}
