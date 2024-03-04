3451e12d91537f0d36b65428623c15327aa42082
root <root@DESKTOP-TTQU7NC>
2024-03-04 22:07:36 +0100

    fonction strctr

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -11,13 +11,13 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i, j;
 
-	if (*needle == '\0') 
+	if (*needle == '\0')
 	{
 		return (haystack);
 	}
 	for (i = 0 ; haystack[i] != '\0' ; i++)
 	{
-		if (haystack [i] != needle[0])
+		if (haystack[i] != needle[0])
 		{
 			continue;
 		}
@@ -28,7 +28,7 @@ char *_strstr(char *haystack, char *needle)
 				break;
 			}
 		}
-		if (needle[j] =='\0')
+		if (needle[j] == '\0')
 		{
 			return (haystack + i);
 		}
