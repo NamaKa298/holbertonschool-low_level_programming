6ee6399c56a760159ef98c135f3b057713ad14b0
root <root@DESKTOP-TTQU7NC>
2024-03-04 15:38:58 +0100

    Première rencontre de lettre

---
+++ pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * _strpbrk - check the code
+ *
+ * @cccept: The character to print
+ * @s: The character to print
+ *
+ * Return: character.
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	char *result;
+
+	for (i = 0 ; s[i] >= '\0' ; i++)
+	{
+		for (j = 0 ; accept[j] >= '\0' ; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				result = &s[i];
+				return (result);
+			}
+		}
+	}
+	return ('\0');
+}
