b5cec722211ef87c582c778f48576290af2740e6
root <root@DESKTOP-TTQU7NC>
2024-03-05 11:35:17 +0100

    somme des diagonales de matrices carrés

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include<stdio.h>
+/**
+ * print_diagsums - check the code
+ *
+ * @a: The character to print
+ * @size: The character to print
+ *
+ * Return: Always nothing.
+ */
+void print_diagsums(int *a, int size)
+{
+	int i;
+
+	int sommediag1 = 0;
+	int sommediag2 = 0;
+
+	for (i = 0 ; i < size ; i++)
+	{
+		sommediag1 += a[size * i + i];
+	}
+	for (i = 1 ; i <= size ; i++)
+	{
+		sommediag2 += a[size * i - i];
+	}
+	printf("%d, %d\n", sommediag1, sommediag2); 
+}
