95479aa56ce863e856b1d8525c563d42faac2f1a
root <root@DESKTOP-TTQU7NC>
2024-03-05 16:00:33 +0100

    Recursion inversée

--- recursion/1-print_rev_recursion.c
+++ recursion/1-print_rev_recursion.c
@@ -9,12 +9,12 @@
 void _print_rev_recursion(char *s)
 {
 	if (*s != '\0')
-        {
-                _print_rev_recursion(s + 1);
-        }
+	{
+		_print_rev_recursion(s + 1);
+	}
 	else
 	{
 		return;
 	}
-        _putchar(*s);
+	_putchar(*s);
 }
