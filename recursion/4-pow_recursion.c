9583430b35494f8f72198e6f481077f8c853a37c
root <root@DESKTOP-TTQU7NC>
2024-03-05 20:27:29 +0100

    Fonction pui ssance récursive

--- recursion/4-pow_recursion.c
+++ recursion/4-pow_recursion.c
@@ -8,13 +8,13 @@
  */
 int _pow_recursion(int x, int y)
 {
-        if (y < 0 || x == 0)
-        {
-                return (-1);
-        }
-        else if (y == 0 || x == 1)
-        {
-                return (1);
-        }
-        return (x * _pow_recursion(x, y - 1));
+	if (y < 0 || x == 0)
+	{
+		return (-1);
+	}
+	else if (y == 0 || x == 1)
+	{
+		return (1);
+	}
+	return (x * _pow_recursion(x, y - 1));
 }
