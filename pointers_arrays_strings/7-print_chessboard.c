f0104851e963046545770d80edfa3f8de705e164
root <root@DESKTOP-TTQU7NC>
2024-03-05 09:41:20 +0100

    prints the chessboard

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_chessboard - check the code
+ *
+ * Return: Always 0.
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+	int k = 0;
+
+	while (*a[k] != '\0')
+	{
+		k++;
+	}
+	for (i = 0 ; i < k ; i++)
+	{
+		for (j = 0 ; j < 8 ; j++)
+		{
+		_putchar(a[i][j]);
+		}
+		_putchar('\n');
+	}
+}
