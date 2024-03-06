cbaa63302ba0baaa75d34c0d8505c7b79e6d34c9
root <root@DESKTOP-TTQU7NC>
2024-03-06 14:21:17 +0100

    Recherche sur l_entrée est un nombre premier

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,40 @@
+/**
+ * checker - check the code
+ *
+ * @n: base number to check
+ *
+ * Return: Always puissance.
+ */
+/**
+ * is_prime_number - check the code
+ *
+ * @nombre_diviseur: number is squared and compared against base
+ * @valeur_test: base number to check
+ *
+ * Return: Always puissance.
+ */  
+
+int checker(int nombre_diviseur, int valeur_test)
+{
+	if (valeur_test <= 1)
+	{
+		return (0);
+	}
+	else if (nombre_diviseur == 1)
+	{
+		return (1);
+	}
+	else if (valeur_test % nombre_diviseur == 0)
+	{
+		return (0);
+	}
+	else
+	{
+		return (checker(nombre_diviseur - 1, valeur_test));
+	}
+}
+
+int is_prime_number(int n)
+{
+	return (checker(n - 1, n));
+}
