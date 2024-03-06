5d8e81af2099d8479ae0c1c681ed4ddd2949c13f
root <root@DESKTOP-TTQU7NC>
2024-03-06 16:23:05 +0100

    Calcule le nombre de fichiers

---
+++ argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+/**
+ * main - check the code
+ *
+ * @argc: character to be verified
+ * @argv: character to be verified
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	if (argv != 0)
+	{	
+		printf("%d\n", argc - 1);
+	}
+	return (0);
+}
