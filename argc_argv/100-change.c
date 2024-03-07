9b1b26a96d49862d761f92b16479e4b3feacc2e8
root <root@DESKTOP-TTQU7NC>
2024-03-07 20:06:14 +0100

    Nombre de pièces à rendre

--- argc_argv/100-change.c
+++ argc_argv/100-change.c
@@ -25,12 +25,10 @@ int main(int argc, char *argv[])
 	number = atoi(argv[1]);
 	for (count = 0 ; count < 5 ; count ++)
 	{
-		printf("valeur restante : %d\n", number);
 		nbre_piece += (number / pieces[count]);
 		cents += nbre_piece;
 		number = number - (nbre_piece * pieces[count]);
 		nbre_piece = 0;
-		printf("valeur restante : %d\n", number);
 		if (number == 0)
 		{
 			printf("%d\n", cents);
