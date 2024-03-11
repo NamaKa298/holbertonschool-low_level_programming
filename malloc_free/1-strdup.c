2201d41703c97195510bc892b9e5f0cf45eb2b09
root <root@DESKTOP-TTQU7NC>
2024-03-11 15:39:25 +0100

    returns a pointer to a newly allocated space in memory, which contains a copy of the string given as a parameter

---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,40 @@
+#include<stdlib.h>
+#include<stdio.h>
+#include"main.h"
+/**
+ * _strdup - check the code
+ *
+ * @str: The character to print
+ *
+ * Return: character.
+ */
+char *_strdup(char *str)
+{
+	char *str_duplicated;
+	unsigned int len, i;
+	char *temporaire = str;
+
+	if (str == 0)
+	{
+		return (NULL);
+	}
+	len = 0;
+	while (*temporaire != '\0')
+	{
+		temporaire++;
+		len++;
+	}
+	str_duplicated = malloc(sizeof(char) * (len + 1));
+	if (str_duplicated == NULL)
+	{
+		return (NULL);
+	}
+	i = 0;
+	while (i < len)
+	{
+		str_duplicated[i] = str[i];
+		i++;
+	}
+	str_duplicated[i] = '\0';
+	return (str_duplicated);
+}
