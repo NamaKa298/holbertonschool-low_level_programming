41aaf7c037cd7037191b5e399cac67634d5bcc76
root <root@DESKTOP-TTQU7NC>
2024-03-12 12:05:11 +0100

    function that returns a pointer to a 2 dimensional array of integers

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -25,7 +25,7 @@ int **alloc_grid(int width, int height)
 		tableau[i] = malloc(sizeof(int) * width);
 		if (tableau[i] == NULL)
 		{
-			for ( ; i >= 0 ; i--)
+			for (; i >= 0 ; i--)
 			{
 				free(tableau[i]);
 			}
