953604b92ba20148479454b2888a519b068ccb26
root <root@DESKTOP-TTQU7NC>
2024-03-13 16:20:41 +0100

    Define a new type struct dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,14 @@
+/**
+ * struct dog - Structure representing a dog
+ * @name: The name of the dog (string)
+ * @age: The age of the dog (float)
+ * @owner: The owner of the dog (string)
+ *
+ * Description: This structure represents a dog and contains
+ * information about its name, age, and owner.
+ */
 struct dog {
-   char *name;
-   float age;
-   char *owner;
-   /* declare as many members as desired, but the entire structure size must be known to the compiler. */
+	char *name;
+	float age;
+	char *owner;
 };
