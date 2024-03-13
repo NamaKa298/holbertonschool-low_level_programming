b4272cad29df6329c4623751302a07644dece0b6
root <root@DESKTOP-TTQU7NC>
2024-03-13 16:24:05 +0100

    Define a new type struct dog

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -5,7 +5,7 @@
  * @owner: The owner of the dog (string)
  *
  * Description: This structure represents a dog and contains
- *              information about its name, age, and owner.
+ * information about its name, age, and owner.
  */
 struct dog
 {
