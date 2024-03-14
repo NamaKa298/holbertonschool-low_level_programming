d0973e7df96a890642420485c4909e490a2239c6
root <root@DESKTOP-TTQU7NC>
2024-03-14 15:45:53 +0100

    function that creates a new dog

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -22,7 +22,7 @@ dog_t *new_dog(char *name, float age, char *owner)
 {
 	struct dog *new_dog;
 
-	new_dog = malloc(sizeof(dog_t));
+	new_dog = malloc(sizeof(struct dog));
 
 	if (new_dog == NULL)
 	{
