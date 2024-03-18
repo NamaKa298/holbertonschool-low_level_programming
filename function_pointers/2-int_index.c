e764b1ff9ec54eafbbd1a1e46172dd8addb4bd2c
root <root@DESKTOP-TTQU7NC>
2024-03-18 14:13:01 +0100

    function that searches for an integer

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -23,6 +23,14 @@ int int_index(int *array, int size, int (*cmp)(int))
 	{
 		return (-1);
 	}
+	if (array == NULL)
+	{
+		return (-1);
+	}
+	if (*cmp == NULL)
+	{
+		return (-1);
+	}
 	for (i = 0 ; i < size ; i++)
 	{
 
