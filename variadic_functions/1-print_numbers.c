da271b2cc672952a14b38832d324e9d78dd9129d
root <root@DESKTOP-TTQU7NC>
2024-03-19 21:51:09 +0100

    function that prints numbers, followed by a new line

--- variadic_functions/1-print_numbers.c
+++ variadic_functions/1-print_numbers.c
@@ -25,7 +25,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 		}
 		else
 		{
-			printf("%c%d", *separator, va_arg(valeurs, int));
+			printf("%c %d", *separator, va_arg(valeurs, int));
 		}
 	}
 	printf("\n");
