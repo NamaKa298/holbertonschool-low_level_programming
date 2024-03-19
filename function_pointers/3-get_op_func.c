5ed944176e70ebc22db95d008d2fdbb4973a3320
root <root@DESKTOP-TTQU7NC>
2024-03-19 12:15:44 +0100

    function that selects the correct function to perform the operation asked by the user

---
+++ function_pointers/3-get_op_func.c
@@ -0,0 +1,47 @@
+#include "3-calc.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * get_op_func - Get the appropriate arithmetic operation function.
+ * @s: The arithmetic operator as a string.
+ *
+ * Return: A pointer to the function performing the arithmetic operation
+ * specified by @s, or NULL if @s does not match any of the expected operators.
+ *
+ * This function takes an arithmetic operator represented as a string
+ * @s and returns
+ * a pointer to the corresponding arithmetic operation function.
+ * The returned
+ * function pointer can then be used to perform the desired arithmetic
+ * operation.
+ * If @s does not match any of the expected operators (+, -, *, /, %),
+ * NULL is returned.
+ */
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (i < 6)
+	{
+		if (*s == *ops[i].op)
+		{
+			if (*s == NULL)
+			{
+				printf("Error\n");
+				exit(99);
+			}
+			return (ops[i].f);
+
+		}
+		i++;
+	}
+	return (0);
+}
