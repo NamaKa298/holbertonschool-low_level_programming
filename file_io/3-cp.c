760bb522816a9ecf0bb6443cb5fe975b8e50dcc0
NamaKa298 <marion.saint-martin@protonmail.com>
2024-04-05 14:37:45 +0200

    program that copies the content of a file to another file

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -17,17 +17,17 @@ int main(int argc, char *argv[])
 	if (argc != 3)
 	{
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		return (97);
+		exit(97);
 	}
 	if (fd_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		return (98);
+		exit(98);
 	}
 	if (fd_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		return (99);
+		exit(99);
 	}
 	while ((bytes_read = read(fd_from, buf, BUF_SIZE)) > 0)
 	{
@@ -35,18 +35,18 @@ int main(int argc, char *argv[])
 		if (bytes_written != bytes_read)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			return (99);
+			exit(99);
 		}
 	}
 	if (bytes_read == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		return (98);
+		exit(98);
 	}
 	if (close(fd_from) == -1 || close(fd_to) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd\n");
-		return (100);
+		exit(100);
 	}
 	return (0);
 }
