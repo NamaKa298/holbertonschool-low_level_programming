051d120511c31d0587a174c8c070e3f39f42f958
NamaKa298 <marion.saint-martin@protonmail.com>
2024-04-05 11:19:49 +0200

    program that copies the content of a file to another file

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -9,7 +9,8 @@
  */
 int main(int argc, char *argv[])
 {
-	int fd_from, fd_to;
+	int fd_from = open(argv[1], O_RDONLY);
+	int fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	ssize_t bytes_read, bytes_written;
 	char buf[BUF_SIZE];
 
@@ -18,13 +19,11 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		return (97);
 	}
-	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		return (98);
 	}
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0644);
 	if (fd_to == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
